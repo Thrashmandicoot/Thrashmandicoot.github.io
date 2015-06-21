f94cce5e88edf44ba17e3f033900af95aa27756f
Thrashmandicoot <Thrashmandicoot@users.noreply.github.com>
2015-06-20 21:56:08 -0700

    adds cheatsheet.html

---
+++ projects/fido.js
@@ -0,0 +1,12 @@
+var fido = {
+  name: "Fido",
+  age: 5,
+  furColor: "White",
+  bark: function(){
+    console.log("wuff");
+  }
+};
+
+console.log(fido.name);
+console.log(fido.age);
+fido.bark();
