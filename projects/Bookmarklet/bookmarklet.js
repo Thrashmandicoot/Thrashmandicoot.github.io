52f9ae4eb79144a0e8af480b21633647d8346f68
Thrashmandicoot <Thrashmandicoot@users.noreply.github.com>
2015-06-26 01:15:34 -0700

    adds bookmarklet html

---
+++ projects/Bookmarklet/bookmarklet.js
@@ -0,0 +1,16 @@
+//non-condensed
+javascript:
+studentArr = ['Thrashmandicoot'],
+git = window.location.href.toString(),
+target = git.split('/')[3],
+newPage = git.replace(target, studentArr[Math.floor(Math.random()*studentArr.length)]);
+window.location.href = newPage;
+
+//SFCohortBookMarklet
+javascript:studentArr=['Thrashmandicoot','ajdlp','ormaechea','atp1o2','anujaverma11','Vrturo','daschi','veed92','emiliocorpus','FelixLobkowicz','gVien','HarinderK','jkarnowski','Jareedos','pborel','Lawrencecs722','lisaswanson','lchan912','mikey2XU','d0ntg0m0ng','robertbaker13','posthum4','TeresaNesteby','user512','Tom2277','traceycarterDBC','zoeingram'],git=window.location.href.toString(),target=git.split('/')[3],newPage=git.replace(target,studentArr[Math.floor(Math.random()*studentArr.length)]);window.location.href=newPage;
+
+//ChiCohortBookmarklet
+javascript:studentArr=['aacarr5','alita42','ccring','dpasie2','DylanAndrews','edougla8','rickkawala','gptasinski','Jasmine-Feldmann','Jay-Quigley-1','ksliu25','linas1985','zebogen','nbkincaid','Nickiam7','SpencerRohan'],git=window.location.href.toString(),target=git.split('/')[3],newPage=git.replace(target,studentArr[Math.floor(Math.random()*studentArr.length)]);window.location.href=newPage;
+
+//All Cohorts
+javascript:studentArr=['ajdlp','ormaechea','atp1o2','anujaverma11','Vrturo','Thrashmandicoot','daschi','veed92','emiliocorpus','FelixLobkowicz','gVien','HarinderK','jkarnowski','Jareedos','pborel','Lawrencecs722','lisaswanson','lchan912','mikey2XU','d0ntg0m0ng','robertbaker13','posthum4','TeresaNesteby','user512','Tom2277','traceycarterDBC','zoeingram','aacarr5','alita42','ccring','dpasie2','DylanAndrews','edougla8','rickkawala','gptasinski','Jasmine-Feldmann','Jay-Quigley-1','ksliu25','linas1985','zebogen','nbkincaid','Nickiam7','SpencerRohan'],git=window.location.href.toString(),target=git.split('/')[3],newPage=git.replace(target,studentArr[Math.floor(Math.random()*studentArr.length)]);window.location.href=newPage;
